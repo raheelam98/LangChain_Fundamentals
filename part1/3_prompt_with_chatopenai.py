9073fd8bbdba91b9e2b400fe2b2ba9c3d00a9be8
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-03 05:37:50 +0500

    prompt with chatopenai

---
+++ part1/3_prompt_with_chatopenai.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Hello LangChain")
+    information = """
+        Karachi is the capital city of the Pakistani province of Sindh. 
+        It is the largest city in Pakistan and 12th largest in the world, with a population of over 20 million
+    """
+
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0)
+
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={"information": information})
+
+    print(res)
