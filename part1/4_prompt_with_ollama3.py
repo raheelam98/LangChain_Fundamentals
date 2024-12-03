7d2f4f061e3d8917fdad59ae752e2b96dbf5a8bf
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-03 21:28:02 +0500

    Create 4_prompt_with_ollama3.py

---
+++ part1/4_prompt_with_ollama3.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+# from langchain_openai import ChatOpenAI
+
+from langchain_ollama import ChatOllama
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Hello LangChain")
+    information = """
+        Karachi is the capital city of the Pakistani province of Sindh. 
+        It is the largest city in Pakistan and 12th largest in the world, with a population of over 20 million
+    """
+
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    #llm = ChatOpenAI(model="gpt-4o",temperature=0)  # open ai model
+    llm = ChatOllama(model = "llama3", )
+
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input={"information": information})
+
+    print(res)
+
+
+
+
+
+
+
+
+
