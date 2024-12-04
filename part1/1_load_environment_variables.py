214a94da88527a9a8252ba3511be3983b37347e1
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-04 16:49:21 +0500

    1_load_environment_variables.py

---
+++ part1/1_load_environment_variables.py
@@ -0,0 +1,16 @@
+# check :-  load env variable to main.py
+import os
+from dotenv import load_dotenv
+
+#Load environment variables from .env file
+load_dotenv()
+
+if __name__ == '__main__':
+    print("hello LangChain")
+    print(os.environ.get('TEST', 'TEST not set'))
+
+# output :- hello LangChain    abc12345
+
+
+# .env  
+# TEST=abc12345
