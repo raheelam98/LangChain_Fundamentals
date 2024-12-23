31810daf08dd44c07645ee3886defcd3f1319d20
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-23 14:59:31 +0500

    Update 0_load_env_variables.py

--- llm_models/0_load_env_variables.py
+++ llm_models/0_load_env_variables.py
@@ -7,4 +7,11 @@ load_dotenv()
 
 if __name__ == '__main__':
     print("hello LangChain")
-    print(os.environ.get('GEMINI_API_KEY', 'GEMINI_API_KEY not set'))
+    print(os.environ.get('TEST_API_KEY', 'TEST_API_KEY not set'))
+
+# output :- hello LangChain    abc12345
+
+
+# .env  
+# TEST_API_KEY=abc12345
+
