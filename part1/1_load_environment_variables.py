602fa7f7c3ae368572247c64bdfe4babe1b22d81
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-23 14:58:09 +0500

    Update 1_load_environment_variables.py

--- part1/1_load_environment_variables.py
+++ part1/1_load_environment_variables.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 if __name__ == '__main__':
     print("hello LangChain") 
-    print(os.environ.get('TEST', 'TEST not set'))
+    print(os.environ.get('TEST', 'TEST not set')) 
 
 # output :- hello LangChain    abc12345
 
