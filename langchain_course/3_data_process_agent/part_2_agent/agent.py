e64e766957509790254d03434be2fc643c7704c7
Raheela <142294662+raheelam98@users.noreply.github.com>
2024-12-31 18:06:20 +0500

    Create agent.py

---
+++ langchain_course/3_data_process_agent/part_2_agent/agent.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+def lookup(name: str):
+    return "https://www.linkedin.com/in/linkedin_name/"
+
+  
+def lookup(name: str):
+    return "https://www.linkedin.com/search/results/people/?keywords=" + name
